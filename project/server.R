313dd5b98dc0434975a5d5f7b2f9a37651ac7416
ericwarren9 <erwarren@ncsu.edu>
2023-11-29 15:19:52 -0500

    Update the app to include many of the graphs for EDA

--- project/server.R
+++ project/server.R
@@ -13,6 +13,23 @@
 library(shiny)
 library(tidyverse)
 library(bslib)
+library(shinyWidgets)
+
+
+# Read data in ------------------------------------------------------------
+
+final_player_info <- read_rds("player_stats_and_salary.rds") %>%
+  mutate(
+    cap_hit_group = ifelse(cap_percent > 10, "high",
+                           ifelse(cap_percent >= 5, "medium", "low")),
+    missed_games = ifelse((as.numeric(year) < 2021) & (games == 16), "no",
+                          ifelse((as.numeric(year) > 2021) & (games == 17), "no", "yes")),
+    cap_hit_group = factor(cap_hit_group, levels = c("low", "medium", "high")),
+    cap_hit_group = relevel(cap_hit_group, "medium"),
+    cap_hit_group = relevel(cap_hit_group, "low"),
+    missed_games = factor(missed_games),
+    year = factor(year)
+  )
 
 
 # Start the server section of app -----------------------------------------
@@ -35,7 +52,7 @@ shinyServer(function(input, output, session) {
     link6 <- a("RDS file", href = "https://github.com/ericwarren9/ST-558-Project-4/blob/main/player_stats_and_salary.rds")
     
     # Make text for this section
-    tagList("To gather this data, we had to use three sources to get the information we wanted on a player's statistics, a player's salary, and the NFL's salary cap maximum in a given season so we know what percent the player was making from the team's budget. Please feel free to observe the corresponding links that will take you to ", link1, ", ", link2, ", and ", link3, ". We then cleaned our data and filtered it in a way that can be used analysis. If you are interested in how this was done (with the corresponding R code), please feel free to check out ", link4, ". If you would like to see the analyzed ", link5, " or the ", link6, " , please feel free to view them as well.")
+    tagList("To gather this data, we had to use three sources to get the information we wanted on a player's statistics, a player's salary, and the NFL's salary cap maximum in a given season so we know what percent the player was making from the team's budget. Please feel free to observe the corresponding links that will take you to ", link1, ", ", link2, ", and ", link3, ". We then cleaned our data and filtered it in a way that can be used analysis. Please note that the player's statistics are on a per game basis so the player will not be penalized more in the analysis for missing more games. This helps make predictoin more accurate and does not weigh the number of games variable even more. Instead this essentially standardizes our data. If you are interested in how this was done (with the corresponding R code), please feel free to check out ", link4, ". If you would like to see the analyzed ", link5, " or the ", link6, " , please feel free to view them as well.")
   })
   
   # Get text for third paragraph for about tab
@@ -56,5 +73,174 @@ shinyServer(function(input, output, session) {
          width = "100%",
          height = "100%")
   }, deleteFile = F)
-
+  
+  # Check which plot is selected
+  plotCheckGraph <- reactive({
+    req("input$var")
+    input$var
+  })
+  
+  # Make facet checkbox condition
+  facetCheck <- reactive({
+    req("input$facetPlot")
+    if(input$facetPlot == 1){
+      "yes"
+    } else {
+      "no"
+    }
+  })
+  
+  # Make facet variable reactive
+  facetVariable <- reactive({
+    req("input$facetChoices")
+    input$facetChoices
+  })
+  
+  # Make boxplot checkbox condition
+  boxplotGroupingCheck <- reactive({
+    req("input$boxplotGrouping")
+    if(input$boxplotGrouping == 1){
+      "yes"
+    } else {
+      "no"
+    }
+  })
+  
+  # Make boxplot grouping variable
+  boxplotGroupingVariable <- reactive({
+    req("input$boxplotChoice")
+    input$boxplotChoice
+  })
+  
+  # Make Boxplot and Histogram regular variable
+  histogramBoxplotVariable <- reactive({
+    req("input$histogramChoice")
+    input$histogramChoice
+  })
+  
+  # Make Barplot regular variable
+  barplotVariable <- reactive({
+    req("input$barplotChoice")
+    input$barplotChoice
+  })
+  
+  # Make barplot checkbox condition
+  barplotGroupingCheck <- reactive({
+    req("input$barplotGrouping")
+    if(input$barplotGrouping == 1){
+      "yes"
+    } else {
+      "no"
+    }
+  })
+  
+  # Make barplot grouping variable
+  barplotGroupingVariable <- reactive({
+    req("input$barplotChoice2")
+    input$barplotChoice2
+  })
+  
+  # Make slider for the number of bins for histogram
+  numberOfBins <- reactive({
+    req("input$bins")
+    input$bins
+  })
+  
+  # Make the correct graph
+  output$graph <- renderPlot({
+    
+    # Get variables
+    plotChosen <- plotCheckGraph()
+    variablePlot <- histogramBoxplotVariable()
+    groupingBoxplotCheckPlot <- boxplotGroupingCheck()
+    groupingBoxplotVariablePlot <- boxplotGroupingVariable()
+    facetCheckPlot <- facetCheck()
+    facetVariablePlot <- facetVariable()
+    barplotVariablePlot <- barplotVariable()
+    groupingBarplotCheckPlot <- barplotGroupingCheck()
+    groupingBarplotVariablePlot <- barplotGroupingVariable()
+    numberOfHistogramBins <- numberOfBins()
+    
+    # Make boxplot
+    if(plotChosen == "Boxplot"){
+      # Make graph based on conditions
+      if(groupingBoxplotCheckPlot == "no") {
+        if(facetCheckPlot == "no") {
+          final_player_info %>%
+            ggplot(aes_string(y = variablePlot)) +
+            geom_boxplot() +
+            theme_bw()
+        } else {
+          final_player_info %>%
+            ggplot(aes_string(y = variablePlot)) +
+            geom_boxplot() +
+            facet_wrap(~ get(facetVariablePlot), labeller = label_value) +
+            theme_bw()
+        }
+      } else {
+        if(facetCheckPlot == "no") {
+          final_player_info %>%
+            ggplot(aes_string(x = groupingBoxplotVariablePlot, y = variablePlot, fill = groupingBoxplotVariablePlot)) +
+            geom_boxplot() +
+            guides(fill = "none") +
+            theme_bw()
+        } else {
+          final_player_info %>%
+            ggplot(aes_string(x  = groupingBoxplotVariablePlot, y = variablePlot, fill = groupingBoxplotVariablePlot)) +
+            geom_boxplot() +
+            guides(fill = "none") +
+            facet_wrap(~ get(facetVariablePlot), labeller = label_value) +
+            theme_bw()
+        }
+      }
+      # Now make the barplot graph
+    } else if(plotChosen == "Barplot") {
+      # Make graph based on conditions
+      if(groupingBarplotCheckPlot == "no") {
+        if(facetCheckPlot == "no") {
+          final_player_info %>%
+            ggplot(aes_string(x = barplotVariablePlot)) +
+            geom_bar() +
+            theme_bw()
+        } else {
+          final_player_info %>%
+            ggplot(aes_string(x = barplotVariablePlot)) +
+            geom_bar() +
+            facet_wrap(~ get(facetVariablePlot), labeller = label_value) +
+            theme_bw()
+        }
+      } else {
+        if(facetCheckPlot == "no") {
+          final_player_info %>%
+            ggplot(aes_string(x = barplotVariablePlot, fill = groupingBarplotVariablePlot)) +
+            geom_bar() +
+            theme_bw()
+        } else {
+          final_player_info %>%
+            ggplot(aes_string(x = barplotVariablePlot, fill = groupingBarplotVariablePlot)) +
+            geom_bar() +
+            facet_wrap(~ get(facetVariablePlot), labeller = label_value) +
+            theme_bw()
+        }
+      }
+    } else if(plotChosen == "Histogram") {
+      if(facetCheckPlot == "no") {
+        final_player_info %>%
+          ggplot(aes_string(x = variablePlot)) +
+          geom_histogram(bins = numberOfHistogramBins) +
+          theme_bw()
+      } else {
+        final_player_info %>%
+          ggplot(aes_string(x = variablePlot)) +
+          geom_histogram(bins = numberOfHistogramBins) +
+          facet_wrap(~ get(facetVariablePlot), labeller = label_value) +
+          theme_bw()
+      }
+    } else {
+      NULL
+    }
+    
+  })
+    
+  
 })
--- project/ui.R
+++ project/ui.R
@@ -13,6 +13,23 @@
 library(shiny)
 library(tidyverse)
 library(bslib)
+library(shinyWidgets)
+
+
+# Read data in ------------------------------------------------------------
+
+final_player_info <- read_rds("player_stats_and_salary.rds") %>%
+  mutate(
+    cap_hit_group = ifelse(cap_percent > 10, "high",
+                           ifelse(cap_percent >= 5, "medium", "low")),
+    missed_games = ifelse((as.numeric(year) < 2021) & (games == 16), "no",
+                          ifelse((as.numeric(year) > 2021) & (games == 17), "no", "yes")),
+    cap_hit_group = factor(cap_hit_group, levels = c("low", "medium", "high")),
+    cap_hit_group = relevel(cap_hit_group, "medium"),
+    cap_hit_group = relevel(cap_hit_group, "low"),
+    missed_games = factor(missed_games),
+    year = factor(year)
+  )
 
 
 # The application ---------------------------------------------------------
@@ -60,25 +77,331 @@ ui <- fluidPage(
         # graphing tab
         tabPanel(
           "Different EDA Graphs",
+          sidebarLayout(
+            sidebarPanel(
+              h3(
+                "Select the Exploratory Data Analysis Features You Would Like to See"
+              ),
+              sliderInput(
+                "slider1", "QB's Cap Hit for Particular NFL Season",
+                min = min(final_player_info$cap_percent),
+                max = max(final_player_info$cap_percent),
+                value = c(min(final_player_info$cap_percent),
+                          max(final_player_info$cap_percent))
+              ),
+              sliderInput(
+                "slider2", "QB's Passing Yards per Game for Particular NFL Season",
+                min = min(final_player_info$passing_yards_per_game),
+                max = max(final_player_info$passing_yards_per_game),
+                value = c(min(final_player_info$passing_yards_per_game),
+                          max(final_player_info$passing_yards_per_game))
+              ),
+              sliderInput(
+                "slider3", "QB's Rushing Yards per Game for Particular NFL Season",
+                min = min(final_player_info$rushing_yards_per_game),
+                max = max(final_player_info$rushing_yards_per_game),
+                value = c(min(final_player_info$rushing_yards_per_game),
+                          max(final_player_info$rushing_yards_per_game))
+              ),
+              pickerInput(
+                "picker1", "Select NFL Season(s)",
+                choices = str_sort(unique(final_player_info$year), decreasing = T),
+                selected = unique(final_player_info$year),
+                options = list(`actions-box` = TRUE,
+                               create = FALSE,
+                               placeholder = "Please Select a Position",
+                               onDropdownOpen = I("function($dropdown) {if (!this.lastQuery.length) {this.close(); this.settings.openOnFocus = false;}}"),
+                               onType = I("function (str) {if (str === \"\") {this.close();}}"),
+                               onItemAdd = I("function() {this.close();}")),
+                multiple = T
+              ),
+              selectInput(
+                "var", "Graphical Display to View",
+                choices = c(
+                  "Scatterplot",
+                  "Barplot",
+                  "Histogram",
+                  "Boxplot"
+                ),
+                selected = "Scatterplot"
+              ),
+              # Make condition on graph selected checked
+              conditionalPanel(
+                condition = "input.var == 'Barplot'",
+                selectInput(
+                  "barplotChoice", "Pick the Variable to Display",
+                  choices = c(
+                    colnames(dplyr::select_if(final_player_info, is.factor))
+                  ),
+                  selected = "year"
+                )
+              ),
+              conditionalPanel(
+                condition = "['Histogram', 'Boxplot'].indexOf(input.var) > -1",
+                selectInput(
+                  "histogramChoice", "Pick the Variable to Display",
+                  choices = c(
+                    colnames(dplyr::select_if(final_player_info, is.numeric))
+                  ),
+                  selected = "passing_yards_per_game"
+                )
+              ),
+              conditionalPanel(
+                condition = "input.var == 'Boxplot'",
+                checkboxInput(
+                  "boxplotGrouping", "Do you want a boxplot grouping variable?",
+                  value = FALSE
+                )
+              ),
+              conditionalPanel(
+                condition = "input.var == 'Boxplot' && input.boxplotGrouping == 1",
+                selectInput(
+                  "boxplotChoice", "Pick a Grouping Variable to Display",
+                  choices = c(
+                    colnames(dplyr::select_if(final_player_info, is.factor))
+                  ),
+                  selected = "cap_hit_group"
+                )
+              ),
+              conditionalPanel(
+                condition = "input.var == 'Barplot'",
+                checkboxInput(
+                  "barplotGrouping", "Do you want a barplot grouping variable?",
+                  value = FALSE
+                )
+              ),
+              conditionalPanel(
+                condition = "input.var == 'Barplot' && input.barplotGrouping == 1",
+                selectInput(
+                  "barplotChoice2", "Pick a Grouping Variable to Display",
+                  choices = c(
+                    colnames(dplyr::select_if(final_player_info, is.factor))
+                  ),
+                  selected = "cap_hit_group"
+                )
+              ),
+              conditionalPanel(
+                condition = "input.var == 'Histogram'",
+                sliderInput(
+                  "bins", "Select the Number of Bins for the Histogram",
+                  min = 5,
+                  max = 30,
+                  value = 10
+                )
+              ),
+              conditionalPanel(
+                condition = "input.var == 'Scatterplot'",
+                selectInput(
+                  "scatterChoice1", "Pick the Variable to Display on the x-axis",
+                  choices = c(
+                    colnames(dplyr::select_if(final_player_info, is.numeric))
+                  ),
+                  selected = "passing_yards_per_game"
+                ),
+                selectInput(
+                  "scatterChoice2", "Pick the Variable to Display on the y-axis",
+                  choices = c(
+                    colnames(dplyr::select_if(final_player_info, is.numeric))
+                  ),
+                  selected = "passing_tds_per_game"
+                ),
+                checkboxInput(
+                  "scatterCheck1", "Do you want to change the color of points based on a variable?",
+                  value = FALSE
+                ),
+                checkboxInput(
+                  "scatterCheck2", "Do you want to change the size of points based on a variable?",
+                  value = FALSE
+                )
+              ),
+              checkboxInput(
+                "facetPlot", 
+                "Do you want to separate (facet) our plot by another variable?",
+                value = FALSE
+              ),
+              conditionalPanel(
+                condition = "input.scatterCheck1 == 1 && input.var == 'Scatterplot'",
+                selectInput(
+                  "scatterChoice3", "Pick the Variable to Display as the color",
+                  choices = c(
+                    colnames(final_player_info)
+                  ),
+                  selected = "cap_hit_group"
+                )
+              ),
+              conditionalPanel(
+                condition = "input.scatterCheck2 == 1 && input.var == 'Scatterplot'",
+                selectInput(
+                  "scatterChoice4", "Pick the Variable to Display as the size",
+                  choices = c(
+                    colnames(final_player_info)
+                  ),
+                  selected = "turnovers_per_game"
+                )
+              ),
+              conditionalPanel(
+                condition = "input.facetPlot == 1",
+                selectInput(
+                  "facetChoices", "Pick a variable to facet our plots on",
+                  choices = c(
+                    colnames(dplyr::select_if(final_player_info, is.factor))
+                  ),
+                  selected = "missed_games"
+                )
+              )
+            ),
+            mainPanel(
+              plotOutput("graph")
+            )
+          )
+        ),
+        
+        # Summary tab
+        tabPanel(
+          "Different EDA Summaries",
           sidebarPanel(
             h3(
               "Select the Exploratory Data Analysis Features You Would Like to See"
             ),
+            sliderInput(
+              "slider1", "QB's Cap Hit for Particular NFL Season",
+              min = min(final_player_info$cap_percent),
+              max = max(final_player_info$cap_percent),
+              value = c(min(final_player_info$cap_percent),
+                        max(final_player_info$cap_percent))
+            ),
+            sliderInput(
+              "slider2", "QB's Passing Yards per Game for Particular NFL Season",
+              min = min(final_player_info$passing_yards_per_game),
+              max = max(final_player_info$passing_yards_per_game),
+              value = c(min(final_player_info$passing_yards_per_game),
+                        max(final_player_info$passing_yards_per_game))
+            ),
+            sliderInput(
+              "slider3", "QB's Rushing Yards per Game for Particular NFL Season",
+              min = min(final_player_info$rushing_yards_per_game),
+              max = max(final_player_info$rushing_yards_per_game),
+              value = c(min(final_player_info$rushing_yards_per_game),
+                        max(final_player_info$rushing_yards_per_game))
+            ),
+            pickerInput(
+              "picker1", "Select NFL Season(s)",
+              choices = str_sort(unique(final_player_info$year), decreasing = T),
+              selected = unique(final_player_info$year),
+              options = list(`actions-box` = TRUE,
+                             create = FALSE,
+                             placeholder = "Please Select a Position",
+                             onDropdownOpen = I("function($dropdown) {if (!this.lastQuery.length) {this.close(); this.settings.openOnFocus = false;}}"),
+                             onType = I("function (str) {if (str === \"\") {this.close();}}"),
+                             onItemAdd = I("function() {this.close();}")),
+              multiple = T
+            ),
             selectInput(
-              "var", "Graphical Display to View",
+              "var2", "Numeric Display to View",
               choices = c(
-                "Scatter Plot" = "scatter",
-                "Bar Plot" = "bar",
-                "Histogram" = "hist",
-                "Box Plot" = "box"
+                "Five Number Summary",
+                "Contingency Table",
+                "Mean and Standard Deviation",
+                "Quantiles"
               ),
-              selected = "scatter"
+              selected = "Five Number Summary"
+            ),
+            # Make condition on graph selected checked
+            conditionalPanel(
+              condition = "['Five Number Summary', 'Mean and Standard Deviation', 'Quantiles'].indexOf(input.var2) > -1",
+              selectInput(
+                "numericChoice1", "Pick the Variable to Get Summary On",
+                choices = c(
+                  colnames(dplyr::select_if(final_player_info, is.numeric))
+                ),
+                selected = "passing_yards_per_game"
+              ),
+              checkboxInput(
+                "numericCheckbox1", 
+                "Would you like to add a grouping variable?",
+                value = FALSE
+              )
+            ),
+            conditionalPanel(
+              condition = "input.numericCheckbox1 == 1 && ['Five Number Summary', 'Mean and Standard Deviation', 'Quantiles'].indexOf(input.var2) > -1",
+              selectInput(
+                "groupingVariablesForSummaries", 
+                "Select your grouping variable",
+                choices = c(
+                  colnames(dplyr::select_if(final_player_info, is.factor))
+                ),
+                selected = "cap_hit_group"
+              )
+            ),
+            conditionalPanel(
+              condition = "input.var2 == 'Contingency Table'",
+              selectInput(
+                "contingencyTableSize",
+                "What kind of contingency table do you want?",
+                choices = c(
+                  "One Way", "Two Way", "Three Way"
+                ),
+                selected = "One Way"
+              )
+            ),
+            conditionalPanel(
+              condition = "input.var2 == 'Contingency Table' && input.contingencyTableSize == 'One Way'",
+              selectInput(
+                "oneWay",
+                "Select the Variable for Your One Way Table",
+                choices = c(
+                  colnames(dplyr::select_if(final_player_info, is.factor))
+                ),
+                selected = "cap_hit_group"
+              )
+            ),
+            conditionalPanel(
+              condition = "input.var2  ==  'Contingency Table' && input.contingencyTableSize == 'Two Way'",
+              selectInput(
+                "twoWay1",
+                "Select the First Variable for Your Two Way Table",
+                choices = c(
+                  colnames(dplyr::select_if(final_player_info, is.factor))
+                ),
+                selected = "cap_hit_group"
+              ),
+              selectInput(
+                "twoWay2",
+                "Select the Second Variable for Your Two Way Table",
+                choices = c(
+                  colnames(dplyr::select_if(final_player_info, is.factor))
+                ),
+                selected = "missed_games"
+              )
+            ),
+            conditionalPanel(
+              condition = "input.var2  ==  'Contingency Table' && input.contingencyTableSize == 'Three Way'",
+              selectInput(
+                "threeWay1",
+                "Select the First Variable for Your Three Way Table",
+                choices = c(
+                  colnames(dplyr::select_if(final_player_info, is.factor))
+                ),
+                selected = "cap_hit_group"
+              ),
+              selectInput(
+                "threeWay2",
+                "Select the Second Variable for Your Three Way Table",
+                choices = c(
+                  colnames(dplyr::select_if(final_player_info, is.factor))
+                ),
+                selected = "missed_games"
+              ),
+              selectInput(
+                "threeWay3",
+                "Select the Third Variable for Your Three Way Table",
+                choices = c(
+                  colnames(dplyr::select_if(final_player_info, is.factor))
+                ),
+                selected = "year"
+              )
             )
           )
-        ),
-        # Summary tab
-        tabPanel(
-          "Different EDA Summaries"
         )
       )
     ),
